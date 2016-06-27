5d159a0a1a074bfd2fb095cfe2eed5dd56997b71
eman1000 <emanzoelife@gmail.com>
2016-06-27 15:28:34 +0800

    Initial Commit

---
+++ CustomerLoyaltyApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomerLoyaltyApp
+//
+//  Created by Emmancipate Musemwa on 27/06/2016.
+//  Copyright © 2016 Emmancipate Musemwa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
